8daecd960eeb465852908c598d75adac0c6b1bf6
andre4ik3 <62390580+andre4ik3@users.noreply.github.com>
2022-07-09 02:58:40 +0300

    Add support for mod patching - Remove Bee variants from Quark mod
    Holy fucking shit that made me want to puke upon seeing it. Hopefully now none of that disgusting stuff stays anywhere here, and god forbid I get arrested for having that sorta shit on my computer.
    
    On the brighter side, script output is now 300 times more readable, if anyone cares...

--- scripts/apply_patches.ts
+++ scripts/apply_patches.ts
@@ -5,8 +5,70 @@ const exec = (...cmd: string[]) => Deno.run({ cmd, stdout: "null" }).status();
 const woExt = (s: string) => s.split(".").slice(0, -1).join(".").trim();
 const woVer = (s: string) => s.replace(s.match(VERSION)?.at(0)!, "").trim();
 
+const stdout = (t: string) => Deno.stdout.write(new TextEncoder().encode(t));
+
 async function ensure(p: Promise<Deno.ProcessStatus>) {
   if (!(await p).success) throw new Error("Command was not successful");
+}
+
+async function applyPatches(dir: string, ext = "jar") {
+  Deno.chdir(dir);
+
+  // Set up arrays of zips and patches
+  const zips: string[] = [];
+  const patches: string[] = [];
+
+  // List all the files in the datapacks directory.
+  for await (const file of Deno.readDir(dir)) {
+    if (file.name.endsWith(`.${ext}`)) zips.push(woExt(file.name));
+    else if (file.name.endsWith(".patch")) patches.push(woExt(file.name));
+  }
+
+  // Filter the zips to be only the ones that have associated patches
+  const zipsWithPatches = zips.filter((z) => patches.includes(woVer(z)));
+
+  // Ensure backup folder is created
+  const backups = `${wd}/scripts/backups`;
+  await ensureDir(backups);
+
+  for (const zip of zipsWithPatches) {
+    const name = woVer(zip);
+    await stdout(`\n=> Patching ${zip}.${ext}...\n`);
+
+    // Back it up first
+    await stdout(`Backing up original file... `);
+    if (await exists(`${backups}/${zip}.${ext}`)) {
+      await stdout("already backed up.\n");
+    } else {
+      await Deno.copyFile(`${zip}.${ext}`, `${backups}/${zip}.${ext}`);
+      await stdout("done.\n");
+    }
+
+    // Unzip the file
+    await stdout("Unzipping... ");
+    await exec("unzip", `${zip}.${ext}`, "-d", name);
+    await Deno.remove(`${zip}.${ext}`);
+    Deno.chdir(name);
+    await stdout("done.\n");
+
+    // Apply the patch
+    await stdout("Applying patch... ");
+    await exec("patch", "-p1", "-i", `../${name}.patch`);
+    await stdout("done.\n");
+
+    // Zip it back up
+    await stdout("Zipping back up... ");
+    await exec("zip", "-Dr", `../${zip}.${ext}`, ".");
+    Deno.chdir("..");
+    await stdout("done.\n");
+
+    await Deno.remove(name, { recursive: true });
+    console.log(`Successfully patched ${zip}.${ext}!`);
+  }
+
+  console.log(
+    `\n=== Successfully patched ${zipsWithPatches.length} ${ext}s! ===`,
+  );
 }
 
 // Get the working directory by stripping the last 2 components from the path.
@@ -14,52 +76,5 @@ const wd = new URL(import.meta.url).pathname.split("/").slice(0, -2).join("/");
 console.log(`Base directory is: ${wd}`);
 Deno.chdir(wd);
 
-const datapacks = `${wd}/world/datapacks`;
-Deno.chdir(datapacks);
-
-// Set up arrays of zips and patches
-const zips: string[] = [];
-const patches: string[] = [];
-
-// List all the files in the datapacks directory.
-for await (const file of Deno.readDir(datapacks)) {
-  if (file.name.endsWith(".zip")) zips.push(woExt(file.name));
-  else if (file.name.endsWith(".patch")) patches.push(woExt(file.name));
-}
-
-// Filter the zips to be only the ones that have associated patches
-const zipsWithPatches = zips.filter((z) => patches.includes(woVer(z)));
-
-// Ensure backup folder is created
-const backups = `${wd}/scripts/backups`;
-await ensureDir(backups);
-
-for (const zip of zipsWithPatches) {
-  const name = woVer(zip);
-  console.log(`\nPatching ${zip}.zip`);
-
-  // Back it up first
-  console.log(`Backing up original file...`);
-  if (await exists(`${backups}/${zip}.zip`)) console.log("Already backed up.");
-  else await Deno.copyFile(`${zip}.zip`, `${backups}/${zip}.zip`);
-
-  // Unzip the file
-  console.log(`Unzipping...`);
-  await exec("unzip", `${zip}.zip`, "-d", name);
-  await Deno.remove(`${zip}.zip`);
-  Deno.chdir(name);
-
-  // Apply the patch
-  console.log(`Applying patch...`);
-  await exec("patch", "-p1", "-i", `../${name}.patch`);
-
-  // Zip it back up
-  console.log(`Zipping back up...`);
-  await exec("zip", "-Dr", `../${zip}.zip`, ".");
-  Deno.chdir("..");
-
-  await Deno.remove(name, { recursive: true });
-  console.log(`Successfully patched ${zip}.zip!`);
-}
-
-console.log(`Successfully patched ${zipsWithPatches.length} datapacks!`);
+await applyPatches(`${wd}/mods`);
+await applyPatches(`${wd}/world/datapacks`, "zip");
